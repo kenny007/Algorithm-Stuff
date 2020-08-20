cff309099eba7ac3c7144eae238d3e6d1c8621c3
kehinde.adebayo <kehinde.adebayo@ektaco.ee>
2020-08-20 11:59:04 +0300

    Naive first recurring item

--- firstRecurringChar.js
+++ firstRecurringChar.js
@@ -17,4 +17,19 @@ function firstRecurringChar(input) {
   return undefined;
 }
 
-firstRecurringChar([2, 3, 4, 5])
+function firstRecurringCharNaive(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; i < input.length; i++) {
+      if (input[i] === input[j]) {
+        console.log(input[i])
+        return input[i];
+      }
+      console.log(undefined);
+      return undefined;
+    }
+  }
+} // O(n^2)
+
+firstRecurringCharNaive([1, 3, 2, 4, 7])
+
+// firstRecurringChar([2, 3, 4, 5])
