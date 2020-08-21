6a45b0cb5e2b87feff97ee40600aeadf1e60c9fd
kehinde.adebayo <kehinde.adebayo@ektaco.ee>
2020-08-21 15:51:11 +0300

    longest substring + singly linked List

--- firstRecurringChar.js
+++ firstRecurringChar.js
@@ -2,33 +2,32 @@
 // Given an array = [2,5,1,2,3,5,1,2,4] It should return 2
 // Given an array = [2,1,1,2,3,5,1,2,4] It should return 1
 // Given an array = [2,3,4,5] It should return undefined
+// Note on input like [2,5,5,2,3,5,1,2,4] using a nested for loop might be a bit 
+// I remember being told there is a guy that uses hashtable for everything now I get why :D the app probably uses more memory
 
 function firstRecurringChar(input) {
-  let bucket = {}
+  let map = {}
   for (let i = 0; i < input.length; i++) {
-    if (!bucket[input[i]]) {
-      bucket[input[i]] = 1
+    if (map[input[i]]) {
+      return input[i];
     }
     else {
-      console.log(input[i])
-      return input[i]
+      map[input[i]] = i
     }
   }
   return undefined;
-}
+} // 0(n) // space complexity O(1)
 
 function firstRecurringCharNaive(input) {
   for (let i = 0; i < input.length; i++) {
     for (let j = i + 1; i < input.length; i++) {
       if (input[i] === input[j]) {
-        console.log(input[i])
         return input[i];
       }
-      console.log(undefined);
       return undefined;
     }
   }
-} // O(n^2)
+} // O(n^2) 
 
 firstRecurringCharNaive([1, 3, 2, 4, 7])
 
---
+++ heuristics.js
@@ -0,0 +1,7 @@
+// Hash Maps are usually the answer to improve Time Complexity
+// If it's a sorted array, use Binary tree to achieve O(log N). Divide and Conquer - Divide the data set into smaller bits
+// Try Sorting your input
+// Hash tables and precomputed information (i.e sorted) are some of the best ways to optimize your code
+// Look at the Time vs Space tradeoff. Sometimes storing extra space in memory can help the time. (Runtime)
+// If the interviewer is giving you advice/tips/hints. Follow them
+// Space time tradeoffs: Hashtables usually solve this a lot of the times. You can use more space, but you can get a time optimization to the process
