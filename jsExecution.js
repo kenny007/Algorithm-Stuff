23033016e8e386b01f308dda5df4405006b4aa93
kehinde.adebayo <kehinde.adebayo@ektaco.ee>
2020-09-24 01:18:39 +0300

    JS execution explanation

---
+++ jsExecution.js
@@ -0,0 +1,16 @@
+// call stack
+console.log('1');
+setTimeout(() => {
+  console.log('2');
+}, 2000);
+console.log('3');
+
+console.log('1');
+// CALL STACK
+
+setTimeout()
+// WEB API
+
+// CALLBACK QUEUE
+
+// EVENT LOOP
